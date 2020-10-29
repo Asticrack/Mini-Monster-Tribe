31569012d26c622a4ea521edddbce02a7343e16c
Alexis MARTIN--COMTE <68051848+AlexisMARTICOMTE@users.noreply.github.com>
2020-10-29 11:37:39 +0100

    première implémentation du selection manager
    le selection manager va gerer les objets selectionné et permettre au joueur de récupérer les informations importantes sur l'objet (stats, action en cours etc...)
    
    ici le joueur peut cliquer sur un objet à la fois et un cube apparait au dessus de l'objet selectionné

--- Assets/scripts/selection_click.cs
+++ Assets/scripts/selection_click.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class selection_click : MonoBehaviour
 {
     public LayerMask clickable_layer;
+    public GameObject currently_selected;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,23 @@ public class selection_click : MonoBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out ray_hit, Mathf.Infinity, clickable_layer))
             {
                 ray_hit.collider.GetComponent<selection_click_on>().clic_me();
+                if (!currently_selected)
+                {
+                    currently_selected = ray_hit.transform.gameObject;
+                }
+                else if (ray_hit.transform.gameObject != currently_selected)
+                {
+                    unselect_currently_selected();
+                    currently_selected = ray_hit.transform.gameObject;
+
+                }
             }
         }
+    }
+
+    private void unselect_currently_selected()
+    {
+        currently_selected.GetComponent<selection_click_on>().destroy_information_text_object();
+        currently_selected.GetComponent<selection_click_on>().is_selected = false;
     }
 }
--- Assets/scripts/selection_click_on.cs
+++ Assets/scripts/selection_click_on.cs
@@ -7,12 +7,14 @@ public class selection_click_on : MonoBehaviour
 {
     public Material red;
     public Material green;
-
+    public Boolean is_selected = false;
+    public GameObject information_text_object;
+    public float z_spawn_factor = 2;
     private MeshRenderer new_render;
     // Start is called before the first frame update
     void Start()
     {
-        new_render = GetComponent<MeshRenderer>();
+        //new_render = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,19 @@ public class selection_click_on : MonoBehaviour
 
     public void clic_me()
     {
-        new_render.material = green;
+        if (!is_selected)
+        {
+            information_text_object = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            information_text_object.transform.position = this.gameObject.transform.position + new Vector3(0, z_spawn_factor, 0);
+            is_selected = true;
+        }
+        
+        //new_render.material = green;
+    }
+
+    public void destroy_information_text_object()
+    {
+        Destroy(information_text_object);
+        information_text_object = null;
     }
 }
