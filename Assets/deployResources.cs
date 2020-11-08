895310fd7909928823bc2fc80c9813c524c974c6
Lola Parisot <lola.parisot98@gmail.com>
2020-11-08 11:40:58 +0100

    feat: Deploy resources randomly inside terrain bounds

---
+++ Assets/deployResources.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployResources : MonoBehaviour
+{
+    public GameObject[] resources;
+    private float width = 12f;
+    private float height = 12f;
+    private float respawnTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fruityBushWave());
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private void spawnFruityBush(int index)
+    {
+        GameObject resourceToInstantiate = resources[index];
+        GameObject a = Instantiate(resourceToInstantiate) as GameObject;
+        a.transform.position = new Vector3(Random.Range(-12f, width), 0f, Random.Range(-12f, height));
+    }
+
+    IEnumerator fruityBushWave()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            int index = Random.Range(0, resources.Length);
+            spawnFruityBush(index);
+        }
+    }
+}
