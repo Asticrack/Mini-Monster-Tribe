45ff7601767e61ae7701d9ce9bafb55f058094cb
Lola Parisot <lola.parisot98@gmail.com>
2020-11-08 12:03:54 +0100

    feat: Tag resources gameObject as "Resource". Spawn resource while nb of resources is < 50

--- Assets/deployResources.cs
+++ Assets/deployResources.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Spawn randomly resources (tree, fruity bush or water) inside terrain boundings.
+ * For instance, it just spawns every 2 seconds.
+ * 
+ **/
 public class deployResources : MonoBehaviour
 {
     public GameObject[] resources;
     private float width = 12f;
     private float height = 12f;
     private float respawnTime = 2.0f;
+    private GameObject[] totalGameObjects;
 
     // Start is called before the first frame update
     void Start()
     {
+        totalGameObjects = GameObject.FindGameObjectsWithTag("Resource");
+        Debug.Log(totalGameObjects.Length + " objects in total at first.");
         StartCoroutine(fruityBushWave());
     }
 
@@ -30,11 +38,13 @@ public class deployResources : MonoBehaviour
 
     IEnumerator fruityBushWave()
     {
-        while(true)
+        while(totalGameObjects.Length < 30)
         {
             yield return new WaitForSeconds(respawnTime);
             int index = Random.Range(0, resources.Length);
             spawnFruityBush(index);
+            totalGameObjects = GameObject.FindGameObjectsWithTag("Resource");
+            Debug.Log(totalGameObjects.Length + " objects in total.");
         }
     }
 }
