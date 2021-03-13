d616ca6405c01f1c76cbb563ae4f277f7fad9cc8
Lola Parisot <lola.parisot98@gmail.com>
2021-03-13 15:42:17 +0100

    creation de la scene pour le module de ressources
    creation du script pour l'eau, chaque lac peut contenir max 5 litres ou plus (pour le moment), et on peut choisir de le vider entièrement, ou d'en prendre une quantité particulière

---
+++ Assets/scripts/Water_ressource.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water_ressource : MonoBehaviour
+{
+    System.Random rand = new System.Random();
+    private float maxWaterQuantity = 5f; // En litres par exemple...
+    private float currentWaterQuantity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateCurrentWaterQuantity();
+    }
+
+    /// <summary>
+    /// Génère la quantité d'eau en litres d'un bassin d'eau, entre 0 et 5 litres, 0 étant impossible.
+    /// </summary>
+    private void generateCurrentWaterQuantity()
+    {
+        currentWaterQuantity = System.Convert.ToSingle(rand.NextDouble()) * 5f;
+        if (currentWaterQuantity < 1f)
+        {
+            generateCurrentWaterQuantity();
+        }
+    }
+
+    public float emptyRessource()
+    {
+        float waterQuantityTaken = currentWaterQuantity;
+        currentWaterQuantity = 0f;
+        return waterQuantityTaken;
+    }
+
+    public float takeRessourceWithQuantity(float quantity)
+    {
+        float waterQuantityTaken;
+        if (quantity > currentWaterQuantity)
+        {
+            waterQuantityTaken = currentWaterQuantity;
+            currentWaterQuantity = 0f;
+            return waterQuantityTaken;
+        } else
+        {
+            waterQuantityTaken = quantity;
+            currentWaterQuantity -= quantity;
+            return waterQuantityTaken;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
